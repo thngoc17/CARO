fb8d801369cb2e365882a164679eec9d5a3a67a1
thngoc17 <148882979+thngoc17@users.noreply.github.com>
2023-10-28 08:40:58 +0700

    Create Menu.h

---
+++ Menu.h
@@ -0,0 +1,128 @@
+#include<stdio.h>
+#include<iostream>
+#include<Windows.h>
+#include<conio.h>
+#include "functions.h"
+void FixConsoleWindow() {
+	HWND consoleWindow = GetConsoleWindow();
+	LONG style = GetWindowLong(consoleWindow, GWL_STYLE);
+	style = style & ~(WS_MAXIMIZEBOX) & ~(WS_THICKFRAME);
+	SetWindowLong(consoleWindow, GWL_STYLE, style);
+}
+
+void GotoXY(int x, int y) {
+	COORD coord;
+	coord.X = x;
+	coord.Y = y;
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+}
+
+
+void ResetData() {
+	for (int i = 0; i < BOARD_SIZE; i++) {
+		for (int j = 0; j < BOARD_SIZE; j++) {
+			_A[i][j].x = 4 * j + LEFT + 2;
+			_A[i][j].y = 2 * i + TOP + 1;
+			_A[i][j].c = 0;
+		}
+	}
+	_TURN = true; _COMMAND = -1;
+	_X = _A[0][0].x; _Y = _A[0][0].y;
+}
+
+void DrawBoard(int pSize) {
+	for (int i = 0; i <= pSize; i++) {
+		for (int j = 0; j <= pSize; j++) {
+			GotoXY(LEFT + 4 * i, TOP + 2 * j);
+			printf(".");
+		}
+	}
+}
+
+void StartGame() {
+	system("cls");
+	ResetData();
+	DrawBoard(BOARD_SIZE);
+}
+
+void GabageCollect() {
+
+}
+
+void ExitGame() {
+	system("cls");
+	GabageCollect();
+}
+
+
+int ProcessFinish(int pWhoWin) {
+	GotoXY(0, _A[BOARD_SIZE - 1][BOARD_SIZE - 1].y + 2);
+	switch (pWhoWin) {
+	case -1:
+		printf("Nguoi choi %d da thang va nguoi choi %d da thua\n", true, false);
+		break;
+	case 1:
+		printf("Nguoi choi %d da thang va nguoi choi %d da thua\n", false, true);
+		break;
+	case 0:
+		printf("Nguoi choi %d da hoa nguoi choi %d\n", false, true);
+		break;
+	case 2:
+		_TURN = !_TURN;
+	}
+	GotoXY(_X, _Y);
+	return pWhoWin;
+}
+int AskContinue() {
+	GotoXY(0, _A[BOARD_SIZE - 1][BOARD_SIZE - 1].y + 4);
+	printf("Nhan 'y/n' de tiep tuc/dung: ");
+	return toupper(_getch());
+}
+
+int TestBoard() {
+	if (_X == 0) return 0;
+	else {
+		if (_X == 1)
+			return (_TURN == true ? -1 : 1); 
+		else return 2;
+	}
+}
+
+
+int CheckBoard(int pX, int pY) {
+	for (int i = 0; i < BOARD_SIZE; i++) {
+		for (int j = 0; j < BOARD_SIZE; j++) {
+			if (_A[i][j].x == pX && _A[i][j].y == pY && _A[i][j].c == 0) {
+				if (_TURN == true) _A[i][j].c = -1; 
+				else _A[i][j].c = 1; 
+				return _A[i][j].c;
+			}
+		}
+	}
+	return 0;
+}
+
+
+void MoveRight() {
+	if (_X < _A[BOARD_SIZE - 1][BOARD_SIZE - 1].x) {
+		_X += 4;
+		GotoXY(_X, _Y);
+	}
+}
+void MoveLeft() {
+	if (_X > _A[0][0].x) {
+		_X -= 4;
+		GotoXY(_X, _Y);
+	}
+}
+void MoveDown() {
+	if (_Y < _A[BOARD_SIZE - 1][BOARD_SIZE - 1].y) {
+		_Y += 2;
+		GotoXY(_X, _Y);
+	}
+}
+void MoveUp() {
+	if (_Y > _A[0][0].y) {
+		_Y -= 2;
+		GotoXY(_X, _Y);
+	}
